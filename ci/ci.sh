8c2c606b355ca39c3dc1a52e24d62b63a02a6834
hungnd8 <hungnd8@vng.com.vn>
2024-05-30 15:04:20 +0700

    updates

--- ci/ci.sh
+++ ci/ci.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-RED="\033[1;31m"
-GREEN="\033[1;32m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
 BLUE="\033[0;34m"
 NO_COLOR="\033[0m"
 
@@ -17,7 +17,7 @@ validate_ref_name () {
         echo "Branch name is qualified: ${ref_name}"
         exit 0
     else
-        echo "Branch name is invalid: ${ref_name}. It should be started with [feature/]"
+        echo "Branch name is denied: ${ref_name}. It should be started with [feature/]"
         exit 1
     fi
 }
@@ -25,7 +25,11 @@ run_tests () {
     cd config && list=$(sed -n 's/^add_executable(//p' CMakeLists.txt | awk -F' ' '{print $1}' | grep "^test") \
     && cmake --build . --target ${list} > /dev/null 2>&1;
     for item in ${list[@]};
-        do printf "\n==============================Test ${item}==============================\n\n";
+        do \
+            test_label="TEST: ${item}"; \
+            printf "${BLUE}${test_label}\n"; \
+            printf '%.0s-' $(seq 1 ${#test_label})}; \
+            printf "\n${NO_COLOR}";
             ../bin/${item};
             if [ $? != 0 ];
                 then printf "${RED}FAILED: ${item}${NO_COLOR}\n";
