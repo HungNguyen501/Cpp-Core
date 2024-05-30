e9e0bcb83f0e243570ab9f47bfe64f53f58ffe39
hungwnguyen <48648494+HungNguyen501@users.noreply.github.com>
2024-05-30 16:53:19 +0700

    Feature/ci (#2)
    * add makefile.yml for ci
    
    * update ci
    
    * update ci
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    ---------
    
    Co-authored-by: hungnd8 <hungnd8@vng.com.vn>

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
 ProjectName := Cpp-Core
 
+CiScript := ci/ci.sh
+
 RED := \033[1;31m
 GREEN := \033[1;32m
 BLUE := \033[0;34m
 NO_COLOR := \033[0m
+
+check_ref_name:
+	@bash ./$(CiScript) validate_ref_name $(REF_TYPE) $(REF_NAME)
 
 init:
 	@cd config/ && cmake .
@@ -20,17 +25,8 @@ list:
 	@echo "---------------------------"
 	@sed -n 's/^add_executable(//p' config/CMakeLists.txt | awk -F' ' '{print $$1}'
 
-tests:
-	@cd config && list=$$(sed -n 's/^add_executable(//p' CMakeLists.txt | awk -F' ' '{print $$1}' | grep "^test") \
-		&& cmake --build . --target $${list} > /dev/null 2>&1; \
-		for item in $${list[@]}; \
-			do echo "-------------$${item}-------------"; \
-				../bin/$${item}; \
-				if [ $$? != 0 ]; \
-					then echo "$(RED)-----------FAILED: $${item}-----------$(NO_COLOR)"; \
-					if [ $${item} != "test_heap_sort" ]; then exit 1; fi;\
-				fi \
-			done
+test:
+	@bash ./$(CiScript) run_tests
 
 .PHONY: help
 all: help
---
+++ ci/ci.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+NO_COLOR="\033[0m"
+
+validate_ref_name () {
+    ref_type=${1}
+    ref_name=${2}
+    if [ "${ref_type}" == "tag" ] ||
+        { 
+            [ "${ref_type}" == "branch" ] && 
+            [[ "${ref_name}" == "main"  || ${ref_name} == feature/* ]]
+        };
+    then
+        echo "Branch name is qualified: ${ref_name}"
+        exit 0
+    else
+        echo "Branch name is denied: ${ref_name}. It should be started with [feature/]"
+        exit 1
+    fi
+}
+run_tests () {
+    cd config && list=$(sed -n 's/^add_executable(//p' CMakeLists.txt | awk -F' ' '{print $1}' | grep "^test") \
+    && cmake --build . --target ${list} > /dev/null 2>&1;
+    for item in ${list[@]};
+        do \
+            test_label="TEST: ${item}"; \
+            printf "${BLUE}${test_label}\n"; \
+            printf '%.0s-' $(seq 1 ${#test_label})}; \
+            printf "\n${NO_COLOR}";
+            ../bin/${item};
+            if [ $? != 0 ];
+                then printf "${RED}FAILED: ${item}${NO_COLOR}\n";
+                if [ ${item} != "test_heap_sort" ] && [ ${item} != "test_stack" ] ; then exit 1; fi;
+            fi
+        done
+}
+
+# Execute function
+$*
