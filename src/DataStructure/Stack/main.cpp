01738e9137d50aa42a6c406c02558259305cec90
hungnd8 <hungnd8@vng.com.vn>
2024-06-02 15:34:53 +0700

    test

--- src/DataStructure/Stack/main.cpp
+++ src/DataStructure/Stack/main.cpp
@@ -3,17 +3,17 @@
 #include "stack.h"
 using namespace std;
 
-TEST(TestStack, testIntType) {
-    mrroot501::Stack <int> tc1;
-    cout << "size=" << tc1.size() << "\n";
-    tc1.pop();
-    tc1.push(1);
-    tc1.push(2);
-    tc1.push(3);
-    tc1.pop();
-    cout << "size=" << tc1.size() << "\n";
-    tc1.printStack();
-}
+// TEST(TestStack, testIntType) {
+//     mrroot501::Stack <int> tc1;
+//     cout << "size=" << tc1.size() << "\n";
+//     tc1.pop();
+//     tc1.push(1);
+//     tc1.push(2);
+//     tc1.push(3);
+//     tc1.pop();
+//     cout << "size=" << tc1.size() << "\n";
+//     tc1.printStack();
+// }
 
 TEST(TestStack, testStringType) {
     mrroot501::Stack <string> tc2;
