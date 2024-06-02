71fa8c5bd71a94a901bef30d73b091434d01ec1f
hungnd8 <hungnd8@vng.com.vn>
2024-06-02 15:44:02 +0700

    test

--- src/DataStructure/Stack/main.cpp
+++ src/DataStructure/Stack/main.cpp
@@ -6,9 +6,6 @@ using namespace std;
 TEST(TestStack, testIntType) {
     mrroot501::Stack <int> tc1;
     cout << "size=" << tc1.size() << "\n";
-    int actual = tc1.size();
-    cout << "size=" << actual << "\n";
-    EXPECT_EQ(actual, 0);
     tc1.pop();
     tc1.push(1);
     tc1.push(2);
@@ -16,6 +13,7 @@ TEST(TestStack, testIntType) {
     tc1.pop();
     cout << "size=" << tc1.size() << "\n";
     tc1.printStack();
+    EXPECT_EQ(0, 0);
 }
 
 int main(int argc, char **argv) {
