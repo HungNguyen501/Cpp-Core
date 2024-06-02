3bdaced58df2f76b0632ba316e384340de9d4822
hungnd8 <hungnd8@vng.com.vn>
2024-06-02 15:38:10 +0700

    test

--- src/DataStructure/Stack/main.cpp
+++ src/DataStructure/Stack/main.cpp
@@ -3,30 +3,17 @@
 #include "stack.h"
 using namespace std;
 
-// TEST(TestStack, testIntType) {
-//     mrroot501::Stack <int> tc1;
-//     cout << "size=" << tc1.size() << "\n";
-//     tc1.pop();
-//     tc1.push(1);
-//     tc1.push(2);
-//     tc1.push(3);
-//     tc1.pop();
-//     cout << "size=" << tc1.size() << "\n";
-//     tc1.printStack();
-// }
-
-TEST(TestStack, testStringType) {
-    mrroot501::Stack <string> tc2;
-    cout << "size=" << tc2.size() << "\n";
-    tc2.pop();
-    tc2.push("a");
-    tc2.push("a");
-    tc2.push("a");
-    tc2.push("a");
-    tc2.push("a");
-    tc2.push("a");
-    tc2.pop();
-    tc2.printStack();
+TEST(TestStack, testIntType) {
+    mrroot501::Stack <int> tc1;
+    cout << "size=" << tc1.size() << "\n";
+    EXPECT_EQ(tc1.size(), 0);
+    tc1.pop();
+    tc1.push(1);
+    tc1.push(2);
+    tc1.push(3);
+    tc1.pop();
+    cout << "size=" << tc1.size() << "\n";
+    tc1.printStack();
 }
 
 int main(int argc, char **argv) {
