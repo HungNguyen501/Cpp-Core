dac2d3e632abe762c2e1cd227bbc930e8ab8b670
hungnd8 <hungnd8@vng.com.vn>
2024-06-02 15:20:43 +0700

    test

--- src/DataStructure/Stack/main.cpp
+++ src/DataStructure/Stack/main.cpp
@@ -5,26 +5,26 @@ using namespace std;
 
 TEST(TestStack, testIntType) {
     mrroot501::Stack <int> s;
-    int actual_size;
-    actual_size = s.size();
+    // int actual_size;
+    // actual_size = s.size();
     // EXPECT_EQ(actual_size, 0);
-    cout << "size=" << actual_size << "\n";
+    cout << "size=" << s.size() << "\n";
     s.pop();
     s.push(1);
     s.push(2);
     s.push(3);
     s.pop();
-    actual_size = s.size();
-    cout << "size=" << actual_size << "\n";
+    // actual_size = s.size();
+    cout << "size=" << s.size() << "\n";
     // EXPECT_EQ(actual_size, 2);
     s.printStack();
 }
 
 TEST(TestStack, testStringType) {
     mrroot501::Stack <string> s;
-    int actual_size;
-    actual_size = s.size();
-    cout << "size=" << actual_size << "\n";
+    // int actual_size;
+    // actual_size = s.size();
+    cout << "size=" << s.size() << "\n";
     // EXPECT_EQ(actual_size, 0);
     s.pop();
     s.push("a");
@@ -34,8 +34,8 @@ TEST(TestStack, testStringType) {
     s.push("a");
     s.push("a");
     s.pop();
-    actual_size = s.size();
-    cout << "size=" << actual_size << "\n";
+    // actual_size = s.size();
+    cout << "size=" << s.size() << "\n";
     // EXPECT_EQ(actual_size, 5);
     s.printStack();
 }
